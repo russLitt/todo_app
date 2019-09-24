1d9f447eb30bd46d42d6cd72570bbd4e0280aa3b
rustic <russellanderson2000@gmail.com>
2019-09-23 21:53:10 -0400

    made todo list title case sensitive

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,4 @@
 class TodoList < ApplicationRecord
     has_many :todo_items, :dependent => :destroy
-    validates :title, uniqueness: true
+    validates :title, uniqueness: {case_sensitive: false}
 end
